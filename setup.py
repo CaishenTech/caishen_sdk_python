42afa1f8c74da1078a0b78afa840676ce5a5720e
Love Trading <philipacho28@gmail.com>
2025-04-17 17:25:19 -0500

    change setup descriptions and fix base url local issue

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="caishen_sdk_python",
     version="0.1.0",
-    description="A simple Hello World Python SDK",
-    author="Your Name",
+    description="The Caishen SDK gives every agent or user access to unlimited multi-chain crypto wallets",
+    author="CaishenTech",
     author_email="hello@caishen.tech",
     url="https://github.com/CaishenTech/caishen_sdk_python/",  # Update with your URL or repository
     packages=find_packages(),
