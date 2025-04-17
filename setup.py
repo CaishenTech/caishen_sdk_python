d969ba250e73d06f53464f4f5ae41f2af7e7d839
Love Trading <philipacho28@gmail.com>
2025-04-17 18:20:33 -0500

    publish sdk 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="caishen_sdk_python",
-    version="0.1.0",
+    version="0.1.2",
     description="The Caishen SDK gives every agent or user access to unlimited multi-chain crypto wallets",
     author="CaishenTech",
     author_email="hello@caishen.tech",
